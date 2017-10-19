72ee87e3136d4e340dcc05f77e58c4133c5586df
Naum Krivoruk <naum.krivoruk@gmail.com>
2017-10-19 18:51:21 +0300

    WIP

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ruckusApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ruckusApp
+ */
+angular.module('ruckusApp')
+  .controller('MainCtrl', ['$scope', 'CommunicationService', function ($scope, CommunicationService) {
+    CommunicationService.initSC();
+
+    $scope.searchTerm = '';
+
+    $scope.getFirstPage = function() {
+      CommunicationService.getTracks($scope.searchTerm).then(
+        function(result){
+          $scope.tracks = result.collection;
+          $scope.hasMore = result.next_href || null;
+        },function (err) {
+          console.log(err);
+          $scope.tracks = [];
+        }
+      );
+    };
+
+    $scope.keyPressed = function(evt) {
+      if (evt.keyCode == 13) {
+        $scope.getFirstPage();
+      }
+    };
+
+    $scope.pageNum = 0;
+    $scope.hasMore = false;
+
+    $scope.getNextTracks = function () {
+      getTracks(++$scope.pageNum);
+    };
+
+    $scope.getPrevTracks = function () {
+      getTracks(--$scope.pageNum);
+    };
+
+    $scope.clearTrackList = function() {
+      $scope.tracks = [];
+    };
+
+    $scope.playTrack = function(track) {
+      CommunicationService.playTrack(track);
+    };
+
+
+    // private functions
+
+    function getTracks(page) {
+      CommunicationService.getNextTracks(page).then(
+        function(result){
+          $scope.tracks = result.collection;
+          $scope.hasMore = result.next_href || null;
+        }, function (err) {
+          console.log(err);
+        }
+      );
+    }
+  }]);
---
+++ app/scripts/services/communication-service.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name ruckusApp.communication
+ * @description
+ * # communication
+ * Service in the ruckusApp.
+ */
+angular.module('ruckusApp')
+  .service('CommunicationService', ['$q', function ($q) {
+    var pageSize = 5;
+    var next_page;
+    var tracks = [];
+    var searchFor;
+
+    this.initSC = function() {
+      SC.initialize({
+        client_id: 'e59d8b005900e38649c1882b87cd828d'
+      });
+    };
+
+    this.getTracks = function(search) {
+      var deferred  = $q.defer();
+      tracks = [];
+      searchFor = search;
+      SC.get('/tracks', { limit: pageSize, q: search, linked_partitioning: 1 }).then(
+        function(result) {
+          next_page = result.next_href.replace('https://api.soundcloud.com', '');
+          deferred.resolve(result);
+      }, function (err) {
+        console.log(err);
+        deferred.reject(err);
+      });
+
+      return deferred.promise;
+    };
+
+    this.getNextTracks = function(page) {
+      var deferred  = $q.defer();
+
+      SC.get('/tracks', { limit: pageSize, q: searchFor, linked_partitioning: 1, offset: pageSize*page }).then(
+        function(result) {
+          next_page = result.next_href;
+          deferred.resolve(result);
+      },function(err){
+        deferred.reject(err);
+      });
+
+      return deferred.promise;
+    };
+
+    this.playTrack = function(track) {
+      SC.oEmbed(track.uri, {
+        element: document.getElementById('player'),
+        auto_play: true
+      });
+      /*SC.stream('/tracks/' + track.id).then(function(player){
+        player.play();
+      });*/
+    }
+  }]);
